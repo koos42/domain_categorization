260dd273a7349ae0a570b86ed71beb08a3862c7d
koos kleven <koos42@gmail.com>
2013-07-14 13:59:22 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PornOrNot::Application.config.secret_token = '5b0834e8660dc7afc5ac802594cc75451312a95e4a740203984a744fb846cf661ba56c9fd3fef722f660898e31193324abc310e14099c8f25d6d9f517a452de5'
