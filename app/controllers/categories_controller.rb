ad0d46fd3a189b75f83418b9c157335a7b37fd47
koos kleven <koos42@gmail.com>
2013-07-15 23:31:19 -0700

    adds views for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,22 +1,30 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def show
+    @category = Category.find(params[:id])
   end
 
   def create
+    Category.new(params[:category]).save
+    redirect_to action: :index and return
   end
 
   def new
+    @category = Category.new
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+    Category.destroy(params[:id])
+    redirect_to action: :index and return
   end
 end
