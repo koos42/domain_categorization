111c39b08b0992c962bbbc8f5fbf682cc9fbe417
koos kleven <koos42@gmail.com>
2013-07-16 02:02:55 -0700

    getting ready for more types of voting

--- app/controllers/domain_category_votes_controller.rb
+++ app/controllers/domain_category_votes_controller.rb
@@ -1,21 +1,22 @@
 class DomainCategoryVotesController < ApplicationController
   before_filter :get_domain_and_category, only: [:vote_for, :vote_against]
-  before_filter :get_voting_method
 
   NUMBER_OF_DOMAINS = 9
 
   def index
-    redirect_to action: :is_it_a
+    actions = %w[ is_it_a which_categories which_domians ]
+    next_action = actions[rand(0..(actions.count - 1))]
+    redirect_to action: next_action
   end
 
   def is_it_a
-    @domain = random_domain 1
+    @domain = random_domains(1).first
     @category = random_category
     @domain_category_vote = DomainCategoryVote.new
   end
 
   def which_categories
-    @domain = random_domain 1
+    @domain = random_domains(1).first
     @categories = Category.all
     @domain_category_vote = DomainCategoryVote.new
   end
@@ -46,12 +47,8 @@ class DomainCategoryVotesController < ApplicationController
     @category = Category.find_by_slug(params[:category_slug])
   end
 
-  def get_voting_method
-    @voting_method = params[:voting_method]
-  end
-
-  def random_domain(max_domains = NUMBER_OF_DOMAINS)
-    Domain.order("RANDOM()").first
+  def random_domains(max_domains = NUMBER_OF_DOMAINS)
+    Domain.order("RANDOM()").limit(max_domains)
   end
 
   def random_category
