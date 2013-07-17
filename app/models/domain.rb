3837e60bbb4c384da85e1ae9301fd585e0066589
koos kleven <koos42@gmail.com>
2013-07-17 11:37:46 -0700

    removed trailing whitespace

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,6 +1,6 @@
 class Domain < ActiveRecord::Base
   validates :name, uniqueness: true, presence: true
- 
+
   has_many :domain_category_votes
   has_many :categories, through: :domain_category_votes
 
